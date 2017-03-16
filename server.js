8bcf03d91f4f21d18ec90d797ddbe2ca8a7eb93f
Rylux <ryckaert.guillaume@neuf.fr>
2017-03-16 13:51:40 +0100

    typo(for real)

--- server.js
+++ server.js
@@ -232,6 +232,6 @@ app.get('/', function(req, res){
 });
         
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port'+ app.get('port'));  
+  console.log('Node app is running on port '+app.get('port'));  
 });
 
