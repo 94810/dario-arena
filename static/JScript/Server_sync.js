c5c0307c524378808a80722cd8bbceda3c20f68b
Pablo Bourdelas <pablo.bourdelas@gmail.com>
2017-03-22 10:09:36 +0100

    lel

--- static/JScript/Server_sync.js
+++ static/JScript/Server_sync.js
@@ -13,8 +13,10 @@ ws.onopen = function(e) {
 	
 		mess = JSON.parse(e.data);
 		if("name" in mess){
+			console.log('NAME');
 			wsSN = mess.name;
 		}else if("newp" in mess){
+			console.log('NEWP');
 			plD[mess.newp] = new Player(new web(mess.ctrl), mess.pos, 50,180,500);	
 		}else if(wsSN in mess){
 			for( pl in mess) if(pl != wsSN){
